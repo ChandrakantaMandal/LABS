8d51916bfb8bb75ee54960ff09c4c6df66c158a9
HIMANSHU KUMAR ROUT <himanshurout136@gmail.com>
2026-07-12 09:22:28 +0530

    feat/otpveri (#14)

--- Server/src/validators/authValidator.js
+++ Server/src/validators/authValidator.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/, 'Password must be at least 8 characters and include uppercase, lowercase, a number, and a special character.'),
   name: z.string().min(2),
 });
 
